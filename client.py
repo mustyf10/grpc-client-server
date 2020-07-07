98f57e4d549fd07b07e33edc5e2d0c292823be2c
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-07-07 17:08:42 +0100

    resolved api.myservice in local hosts file

--- client.py
+++ client.py
@@ -4,12 +4,12 @@ import foobar_pb2
 import foobar_pb2_grpc
 
 def run():
-    channel = grpc.insecure_channel('127.0.0.1:1699')
+    channel = grpc.insecure_channel('api.myservice:1699')
     stub = foobar_pb2_grpc.FoobarStub(channel)
     response = stub.SayHello(foobar_pb2.HelloRequest(name='you'))
     print("Client received: " + response.message)
 
-    channel2 = grpc.insecure_channel('127.0.0.1:1698')
+    channel2 = grpc.insecure_channel('api.myservice:1698')
     stub = foobar_pb2_grpc.FoobarStub(channel2)
     response = stub.SayHello(foobar_pb2.HelloRequest(name='you'))
     print("Client received: " + response.message)
